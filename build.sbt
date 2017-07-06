5958d33793260c0a247194cd150654c3d3609479
Federico D'Ambrosio <fedexist@gmail.com>
2017-07-06 12:54:23 +0200

    Add KafkaTridentSpout and JsonParser

--- build.sbt
+++ build.sbt
@@ -5,9 +5,10 @@ version := "1.0"
 scalaVersion := "2.12.1"
 
 libraryDependencies += "org.apache.storm" % "storm-kafka" % "1.0.1"
-libraryDependencies += "org.apache.storm" % "storm-core" % "1.0.1"
+libraryDependencies += "org.apache.storm" % "storm-core" % "1.0.1" % "provided"
 libraryDependencies += "org.apache.storm" % "storm-hive" % "1.0.1"
 libraryDependencies += "org.apache.storm" % "storm-sql-kafka" % "1.0.1"
+libraryDependencies += "org.apache.kafka" % "kafka_2.11" % "0.10.2.1"
 
 assemblyMergeStrategy in assembly := {
   case PathList("META-INF", xs @ _*) => MergeStrategy.discard
--- src/main/scala/topology/TestTopology.scala
+++ src/main/scala/topology/TestTopology.scala
@@ -6,17 +6,58 @@ import org.apache.storm.hive.bolt.HiveBolt
 import org.apache.storm.hive.bolt.mapper.DelimitedRecordHiveMapper
 import org.apache.storm.hive.common.HiveOptions
 import org.apache.storm.hive.trident.{HiveStateFactory, HiveUpdater}
-import org.apache.storm.kafka.{KafkaSpout, SpoutConfig, ZkHosts}
+import org.apache.storm.kafka.{BrokerHosts, KafkaSpout, SpoutConfig, ZkHosts}
 import org.apache.storm.{Config, StormSubmitter, trident}
 import org.apache.storm.trident.{TridentState, TridentTopology}
 import org.apache.storm.trident.state.StateFactory
-import org.apache.storm.tuple.Fields
+import org.apache.storm.tuple.{Fields, Values}
+import org.apache.storm.kafka.trident.{OpaqueTridentKafkaSpout, TridentKafkaConfig}
+import org.apache.storm.trident.operation.BaseFunction
+import org.apache.storm.trident.operation.TridentCollector
+import org.apache.storm.trident.tuple.TridentTuple
 
 import scala.collection.JavaConverters._
+import scala.util.parsing.json.JSON
 
 
 
 class TestTopology {
+
+
+
+  class ParseJSON extends BaseFunction {
+
+    private val HTTP_CODE_200 = 200
+    /**
+      * Takes a tuple adds the RDNS and emits a new tuple.
+      *
+      * @param tuple     an TridentTuple that contains fields in JSON format
+      * @param collector the TridentCollector
+      **/
+    override final def execute(tuple: TridentTuple, collector: TridentCollector): Unit = {
+      val bytes = tuple.getBinary(0)
+      try {
+        val decoded = new String(bytes)
+        val result = JSON.parseFull(decoded)
+        result match {
+          case Some(json: Map[String, Any]) => collector.emit(new Values( json("origin"),
+                                                                          json("flight"),
+                                                                          json("course"),
+                                                                          json("aircraft"),
+                                                                          json("callsign"),
+                                                                          json("registration"),
+                                                                          json("lat"),
+                                                                          json("speed"),
+                                                                          json("altitude"),
+                                                                          json("destination"),
+                                                                          json("lon"),
+                                                                          json("time")))
+          case None => println("Parsing failed")
+          case other => println("Unknown data structure: " + other)
+        }
+      }
+    }
+  }
 
   def main(args: Array[String]): Unit = {
 
@@ -43,17 +84,17 @@ class TestTopology {
     val hiveBolt = new HiveBolt(hiveOptions)
 
     //KafkaSpout
-    val kafkaConfig = new SpoutConfig(zkHosts, "air_traffic", "", "storm")
-    val kafkaSpout = new KafkaSpout(kafkaConfig)
+    val broker = new ZkHosts("localhost")
+    val spoutConf = new TridentKafkaConfig(broker, "air_traffic")
+    val kafkaSpout = new OpaqueTridentKafkaSpout(spoutConf)
 
     //Topology
     val topology: TridentTopology = new TridentTopology
-    val stream: trident.Stream = topology.newStream("eventsEmitter", kafkaSpout)
-
-
     val factory: StateFactory = new HiveStateFactory().withOptions(hiveOptions)
-    val state: TridentState = stream.partitionPersist(factory, new Fields(colNames), new HiveUpdater(), new Fields())
-    //builder.setBolt("hiveProcessor", hiveBolt, 8).fieldsGrouping("requestsEmitter", new Fields(colNames))
+    val stream: trident.Stream = topology.newStream("jsonEmitter", kafkaSpout)
+                                  .each(new Fields("bytes"), new ParseJSON , new Fields(colNames))
+
+    stream.partitionPersist(factory, new Fields(colNames), new HiveUpdater(), new Fields()).parallelismHint(8)
 
     //Storm Config
     val config = new Config
